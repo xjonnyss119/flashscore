23216d786dfe65e80f98eae8ef29a252c6df97c1
xjonnys <ivanzuk68@gmail.com>
2026-05-18 20:58:48 +0300

    lasthope

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -10,16 +10,17 @@ function generateCode() {
 }
 
 const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST, 
-  port: Number(process.env.SMTP_PORT) || 465,
-  secure: true,
+  host: "94.100.180.160", 
+  port: 465,
+  secure: true, 
   auth: {
     user: process.env.SMTP_USER, 
     pass: process.env.SMTP_PASS, 
   },
   tls: {
     rejectUnauthorized: false,
-  },
+    servername: "smtp.mail.ru"
+  }
 });
 
 async function sendVerificationEmail(toEmail, code) {
