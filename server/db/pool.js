0ac88b75e3c841e752446caf81cef7ced271145a
xjonnys <ivanzuk68@gmail.com>
2026-05-18 18:49:34 +0300

    fix ssl

--- server/db/pool.js
+++ server/db/pool.js
@@ -6,6 +6,9 @@ const pool = new Pool({
   database: process.env.DB_NAME || "flashscore",
   user: process.env.DB_USER || "postgres",
   password: process.env.DB_PASSWORD || "postgres",
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 pool.on("error", (err) => {
